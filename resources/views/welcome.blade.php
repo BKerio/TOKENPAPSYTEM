10e95730a1d7b8cf1492ae22422dde908cc04172
BKerio <briankerio48@gmail.com>
2026-04-07 12:58:27 +0300

    new changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1602,7 +1602,7 @@
                     <li>
                         <a href="https://cloud.tokenpap.com" target="_blank"
                             class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
-                            Check it now
+                            Explore it now
                         </a>
                     </li>
                 </ul>
